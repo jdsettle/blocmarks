b7d4e3450860a5b787b40946ef4a049aa909aeb6
Jonathan Settle <jdsettle@gmail.com>
2015-11-30 21:24:07 -0500

    Progress on receiving incoming mail

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'welcome/index'
 
+  devise_for :users
+
+  get 'welcome/index'
   get 'welcome/about'
+
+  post :incoming, to: 'incoming#create'
 
   root to: 'welcome#index'
 end
