d8a2440babe61ecb3407873b8b3fdee0b442ebc6
Jonathan Settle <jdsettle@gmail.com>
2015-12-10 19:57:04 -0500

    small changes to incoming controller for user creation

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -7,14 +7,17 @@ class IncomingController < ApplicationController
     # Find the user by using params[:sender]
     # Find the topic by using params[:subject]
     # Assign the url to a variable after retreiving it from params["body-plain"]
-    @user = User.find_by_email(params[:sender])
-    @topic = Topic.find_by_title(params[:subject])
+    puts "INCOMING PARAMS HERE: #{params}"
+
+    @user = User.find_by(email: params[:sender])
+    @topic = Topic.find_by(title: params[:subject])
     @url = params["body-plain"]
 
     # Check if user is nil, if so, create and save a new user
     if @user.nil?
       @user = User.new(
         email: params[:sender]
+        password: params[:sender]
       )
       @user.save
     end
